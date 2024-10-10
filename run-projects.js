f413f27d18294693be709fc85103ff55b34005ca
heo001997 <heo001997@gmail.com>
2024-10-10 20:20:06 +0700

    Add projects runner

---
+++ run-projects.js
@@ -0,0 +1,24 @@
+const concurrently = require('concurrently');
+const path = require('path');
+
+concurrently([
+  {
+    command: 'node server.js',
+    name: 'terminal-command-runner',
+    cwd: path.resolve(__dirname, '../terminal-command-runner'),
+    prefixColor: 'blue'
+  },
+  {
+    command: 'npm run start',
+    name: 'ws-scrcpy',
+    cwd: path.resolve(__dirname, '../ws-scrcpy'),
+    prefixColor: 'green'
+  }
+], {
+  prefix: 'name',
+  killOthers: ['failure', 'success'],
+  restartTries: 3,
+}).result.then(
+  () => console.log('All processes exited successfully'),
+  (error) => console.error('One or more processes failed', error)
+);
